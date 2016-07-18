0dc91ebf01981e105e95b34a82a98015838983a2
Jota!! <juanjo.j.12.1@gmail.com>
2016-07-18 01:51:16 -0400

    Segundo Comit de prueba

--- src/buscaminasprogra/NewClass.java
+++ src/buscaminasprogra/NewClass.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package buscaminasprogra;
 
 /**
@@ -10,11 +5,12 @@ package buscaminasprogra;
  * @author Jota
  */
 public class NewClass {
-    public static void main (String Args [])
-    {
+
+    public static void main(String Args[]) {
         Tablero tablero = new Tablero(6);
         tablero.minar(7);
         tablero.imprimir();
+        String Nombre = "";
     }
-    
+
 }
