9e2b5fea5183df4621a9c7a85785b18fd67b0d05
Jota!! <juanjo.j.12.1@gmail.com>
2016-07-21 11:58:24 -0400

    comit 3

---
+++ src/buscaminasprogra/Juego.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscaminasprogra;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Jota
+ */
+public class Juego {
+
+    public static void main(String Args[]) throws IOException {
+        InputStreamReader r = new InputStreamReader(System.in);
+        BufferedReader a = new BufferedReader(r);
+
+        System.out.println();
+        String pos = (a.readLine());
+        System.out.println();
+    }
+}
